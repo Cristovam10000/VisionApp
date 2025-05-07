a6cd530adf13fc1014bb1dc64167fc29e7646056
cristovam <cristovam1712@gmail.com>
2025-05-06 22:20:41 -0300

    Remove arquivos desnecessários e estrutura do projeto, adicionando novos arquivos de tela e configuração do Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import '../firebase_options.dart'; // Você já tem esse arquivo
+import 'firebase_options.dart'; // Você já tem esse arquivo
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
