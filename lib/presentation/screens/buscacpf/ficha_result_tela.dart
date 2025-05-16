f0376fc2a96974667e66aa284347fe56cbdfd581
gustavo <gust.dovale@gmail.com>
2025-05-16 00:12:09 -0300

    ultimas atualizações da tela perfil do criminoso

--- lib/presentation/screens/buscacpf/ficha_result_tela.dart
+++ lib/presentation/screens/buscacpf/ficha_result_tela.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vision_app/core/constants/app_colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FichaResultPage extends StatelessWidget {
   final Map<String, dynamic> ficha;
@@ -8,109 +10,88 @@ class FichaResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF121212), // Fundo escuro
-      appBar: AppBar(
-        title: const Text('Resultado da Ficha'),
-        backgroundColor: const Color(0xFF1E1E1E), // Cor do AppBar
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child:
-            ficha.isEmpty
-                ? const Center(
-                  child: Text(
-                    'Nenhuma informação encontrada.',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                )
-                : ListView(
-                  children: [
-                    // Foto e Nome
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage:
-                              ficha['foto_url'] != null
-                                  ? NetworkImage(
-                                    ficha['foto_url'],
-                                  ) // Carrega a imagem da URL
-                                  : const NetworkImage(
-                                        'https://media.istockphoto.com/id/1495088043/pt/vetorial/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=170667a&w=0&k=20&c=ESgi5k6Bptg59QPXfAzQiWHtNhvMrUVn07x3wM6E4RU=',
-                                      )
-                                      as ImageProvider,
-
-                          /// Substitua pela imagem do perfil
-                        ),
-                        const SizedBox(width: 16),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              ficha['nome'] ?? 'Nome não encontrado',
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 4),
-                            Text(
-                              ficha['vulgo'] ?? 'Nenhum vulgo',
-                              style: const TextStyle(
-                                color: Colors.grey,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 24),
-
-                    // Informações Pessoais
-                    Text(
-                      'CPF: ${ficha['cpf'] ?? 'Não informado'}',
-                      style: const TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Data de Nascimento: ${ficha['data_nascimento'] ?? 'Não informado'}',
-                      style: const TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Nome da Mãe: ${ficha['nome_mae'] ?? 'Não informado'}',
-                      style: const TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Nome do Pai: ${ficha['nome_pai'] ?? 'Não informado'}',
-                      style: const TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    const SizedBox(height: 24),
-
-                    // Resumo Criminal
-                    const Text(
-                      'Resumo Criminal',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-
-                    // Lista de Crimes
-                    if (ficha['crimes'] != null)
-                      ...List.generate((ficha['crimes'] as List).length, (
-                        index,
-                      ) {
-                        final crime = ficha['crimes'][index];
-                        return CrimeCard(crime: crime);
-                      }),
-                  ],
+      backgroundColor: ColorPalette.dark,
+      appBar: AppBar(backgroundColor: ColorPalette.dark),
+      body:
+          ficha.isEmpty
+              ? const Center(
+                child: Text(
+                  'Nenhuma informação encontrada.',
+                  style: TextStyle(color: Colors.white),
                 ),
+              )
+              : ListView(
+                padding: const EdgeInsets.all(24),
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 48,
+                        backgroundImage: NetworkImage(
+                          ficha['foto_url'] ??
+                              'https://i.imgur.com/j6xgQ7D.png',
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        ficha['nome'] ?? 'Nome não encontrado',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        ficha['vulgo'] ?? 'Vulgo não encontrado',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white70,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 8),
+                      SvgPicture.asset(
+                        'assets/Iconperson.svg', // Substitua pelo caminho correto do arquivo SVG
+                        width: 20,
+                        height: 20,
+                        color: Colors.white70, // Aplica uma tonalidade branca
+                      ),
+                      const SizedBox(height: 24),
+                      infoLine('CPF', ficha['cpf']),
+                      infoLine('Data de Nascimento', ficha['data_nascimento']),
+                      infoLine('Nome da Mãe', ficha['nome_mae']),
+                      infoLine('Nome do Pai', ficha['nome_pai']),
+                      const SizedBox(height: 24),
+                      const Text(
+                        'Resumo Criminal',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      if (ficha['crimes'] != null)
+                        ...List.generate(
+                          ficha['crimes'].length,
+                          (index) => CrimeCard(crime: ficha['crimes'][index]),
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+    );
+  }
+
+  Widget infoLine(String label, dynamic value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Text(
+        '$label: ${value ?? 'Não informado'}',
+        style: const TextStyle(color: Colors.white, fontSize: 16),
       ),
     );
   }
@@ -124,9 +105,9 @@ class CrimeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: const Color(0xFF1E1E1E),
-      margin: const EdgeInsets.only(bottom: 16),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      color: const Color.fromRGBO(19, 39, 61, 1),
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
@@ -135,12 +116,12 @@ class CrimeCard extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  crime['nome_crime'] ?? 'Crime não informado',
-                  style: const TextStyle(
+                const Text(
+                  'Mandato',
+                  style: TextStyle(
                     color: Colors.white,
-                    fontSize: 16,
                     fontWeight: FontWeight.bold,
+                    fontSize: 16,
                   ),
                 ),
                 Container(
@@ -151,39 +132,37 @@ class CrimeCard extends StatelessWidget {
                   decoration: BoxDecoration(
                     color:
                         crime['status'] == 'Em Aberto'
-                            ? Colors
-                                .green // Verde para "Em Aberto"
-                            : crime['status'] == 'Foragido'
-                            ? Colors
-                                .red // Vermelho para "Foragido"
-                            : Colors.grey, // Cinza para outros casos
-                    borderRadius: BorderRadius.circular(4),
+                            ? ColorPalette.vermelhoPaleta
+                            : Colors.grey,
+                    borderRadius: BorderRadius.circular(6),
                   ),
                   child: Text(
-                    crime['status'] ?? 'Status desconhecido',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    crime['status'] ?? 'Desconhecido',
+                    style: const TextStyle(color: Colors.white, fontSize: 12),
                   ),
                 ),
               ],
             ),
             const SizedBox(height: 8),
             Text(
+              'Data: ${crime['data'] ?? 'Não informada'}',
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 4),
+            Text(
               'Artigo: ${crime['artigo'] ?? 'Não informado'}',
-              style: const TextStyle(color: Colors.white, fontSize: 14),
+              style: const TextStyle(color: Colors.white),
             ),
             const SizedBox(height: 4),
-            Text(
-              'Descrição: ${crime['descricao'] ?? 'Não informado'}',
-              style: const TextStyle(color: Colors.white, fontSize: 14),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              'Local: ${crime['cidade'] ?? 'Cidade não informada'}, ${crime['estado'] ?? 'Estado não informado'}',
-              style: const TextStyle(color: Colors.white, fontSize: 14),
+            Row(
+              children: [
+                const Icon(Icons.location_on, color: Colors.white70, size: 16),
+                const SizedBox(width: 4),
+                Text(
+                  '${crime['cidade'] ?? ''}, ${crime['estado'] ?? ''}',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
             ),
           ],
         ),
