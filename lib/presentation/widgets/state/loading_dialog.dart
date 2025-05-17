3405aa8fd149a568bbd8a44573e07499c652ab9c
anderson <and.dovale@gmail.com>
2025-05-17 20:51:07 -0300

    loading

--- lib/presentation/widgets/state/loading_dialog.dart
+++ lib/presentation/widgets/state/loading_dialog.dart
@@ -3,26 +3,16 @@ import 'package:flutter/material.dart';
 void showLoadingDialog(BuildContext context, {String? mensagem}) {
   showDialog(
     context: context,
-    barrierDismissible: false, // impede o fechamento tocando fora
+    barrierDismissible: false,
+    barrierColor: const Color(0xFF132D46), // escurece o fundo da tela
     builder: (BuildContext context) {
-      return Dialog(
-        backgroundColor: Colors.black87,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const CircularProgressIndicator(color: Colors.white),
-              if (mensagem != null) ...[
-                const SizedBox(height: 20),
-                Text(
-                  mensagem,
-                  style: const TextStyle(color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
-              ]
-            ],
+      return const Center(
+        child: SizedBox(
+          height: 50,
+          width: 50,
+          child: CircularProgressIndicator(
+            strokeWidth: 3,
+            color: Colors.white,
           ),
         ),
       );
